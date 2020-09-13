0c4d43449d57948c2427861b1a635a39ff815e3c
Mike Occhicone <65381495+mocchicone@users.noreply.github.com>
2020-09-12 22:27:41 -0700

    updated index.js

--- static/js/app.js
+++ static/js/app.js
@@ -10,16 +10,40 @@ d.forEach((ufo) => {
     });
   });
 
-  var button = d3.select("#filter-btn");
-  var form = d3.select("#form");
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
+button.on("click", search);
+form.on("submit", search);
   
-  button.on("click", search);
-  form.on("submit", search);
+function search() {
+  d3.event.preventDefault();
+  tbody.html(" ");
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property("value");
   
-  function search() {
-    d3.event.preventDefault();
-    var inputElement = d3.select("#datetime");
-    var inputValue = inputElement.property("value");
-    console.log(inputValue);
-  }
-   
+  d.forEach((ufo) => {
+    var row = tbody.append("tr");
+    Object.entries(ufo).forEach(([key, value]) => {
+      if (value === inputValue) {
+      var cell = row.append("td");
+      cell.text(value);
+      var cell = row.append("td");
+      cell.text(ufo.city);
+      var cell = row.append("td");
+      cell.text(ufo.state);
+      var cell = row.append("td");
+      cell.text(ufo.country);
+      var cell = row.append("td");
+      cell.text(ufo.shape);
+      var cell = row.append("td");
+      cell.text(ufo.durationMinutes);
+      var cell = row.append("td");
+      cell.text(ufo.comments);      
+      }
+      else 
+      });
+    });
+  console.log(inputValue)
+};
+    
+
